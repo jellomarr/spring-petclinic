0ce0eefe1a5c0d9ed4ca6b53947b2c14fd40fa3c
Eilos <HandM@Hanss-iMac.local>
2022-02-22 22:25:49 +1100

    Jenkin pipeline test

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -20,7 +20,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Testing the Jenkins Pipeline
+ * Testing the Jenkins pipeline 
  *
  * 
  *
